99e7c828096a76c0fd099fdceb3b974dc19787b3
Christian <christianespacoalameda@gmail.com>
2023-05-12 17:12:22 -0300

    prova2

--- provaq1/Program.cs
+++ provaq1/Program.cs
@@ -2,53 +2,19 @@
 {
     private static void Main(string[] args)
     {
-        string[] palavras = { "pedra", "cabelo", "macaco", "computador", "mouse", "rato", "cimento", "teclado" };
-        Random random = new Random();
-        string palavra = palavras[random.Next(palavras.Length)];
-        string palavraemtraço = new string('_', palavra.Length);
-        int chances = 6;
-        while (chances > 0 && palavraemtraço.Contains('_'))
+        string palavra = Console.ReadLine();
+        char[] caracteres = palavra.ToCharArray();
+        Array.Reverse(caracteres);
+        string palavrainvertida = new string(caracteres);
+
+
+        if (palavrainvertida == palavra)
         {
-
-            Console.WriteLine(palavraemtraço);
-            Console.WriteLine("Tentativas restantes: " + chances);
-
-
-            Console.Write("Digite uma letra: ");
-            char letra = Console.ReadLine()[0];
-
-
-            bool acertou = false;
-            for (int i = 0; i < palavra.Length; i++)
-            {
-                if (palavra[i] == letra)
-                {
-                    acertou = true;
-
-
-                    palavraemtraço = palavraemtraço.Substring(0, i) + letra + palavraemtraço.Substring(i + 1);
-                }
-            }
-
-
-            if (acertou)
-            {
-                Console.WriteLine("Acertou!");
-            }
-            else
-            {
-                Console.WriteLine("Errou!");
-                chances--;
-            }
-        }
-
-        if (chances == 0)
-        {
-            Console.WriteLine("Você perdeu! A palavra era: " + palavra);
+            Console.WriteLine("A palavra é um palindromo");
         }
         else
         {
-            Console.WriteLine("Parabéns, você ganhou! A palavra era: " + palavra);
+            Console.WriteLine("A palavra não é  um palindromo");
         }
     }
 }
